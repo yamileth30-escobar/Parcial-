20ab87756849e855cc5d2e8dbb8c9419fd39cca8
yamileth30-escobar <ve1105032024@unab.edu.sv>
2026-03-05 21:06:02 -0600

    Add files via upload

---
+++ Moto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1Programacion1G2
+{
+    public class Moto : Vehiculo
+    {
+        public int Cilindraje { get; set; }
+
+        public Moto(string marca, string modelo, int anio, int cilindraje)
+            : base(marca, modelo, anio)
+        {
+            Cilindraje = cilindraje;
+        }
+
+        public override void MostrarInformacion()
+        {
+            base.MostrarInformacion();
+            Console.WriteLine("Cilindraje: " + Cilindraje);
+        }
+
+        public override void Encender()
+        {
+            Console.WriteLine("La moto se enciende con arranque eléctrico.");
+        }
+    }
+}
